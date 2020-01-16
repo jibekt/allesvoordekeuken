a6891f64da486365cb50aea05962bd63c482b1b1
zhibek.turgumbaeva <zhibekt@gmail.com>
2020-01-16 12:52:13 +0100

    Initial commit

---
+++ src/test/java/be/vdab/allesvoordekeuken/repositories/DataSourceTest.java
@@ -0,0 +1,21 @@
+package be.vdab.allesvoordekeuken.repositories;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@DataJpaTest
+public class DataSourceTest {
+    private final DataSource dataSource;
+    DataSourceTest(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+    @Test
+    void getConnection() throws SQLException {
+        try (Connection connection = dataSource.getConnection()) {
+        }
+    }
+}
